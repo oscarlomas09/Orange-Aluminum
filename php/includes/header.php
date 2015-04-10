8c5624c8339b2de7c902ca856743e3bc9c9246f2
oscarlomas09 <oscar-lomas09@hotmail.com>
2015-04-10 10:17:06 -0700

    Foundation Files Added

---
+++ php/includes/header.php
@@ -0,0 +1,10 @@
+<header>
+    <div class="promo">Free Ground Shipping on Orders of $200+</div>
+    <nav class="navbar">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a href="" class="navbar-brand"><img src="" alt=""></a>
+            </div>
+        </div>
+    </nav>
+</header>
