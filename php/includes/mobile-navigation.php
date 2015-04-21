bbadcffb520cacf596c8d42c820426d6a93ac2ba
oscarlomas09 <oscar-lomas09@hotmail.com>
2015-04-21 16:09:02 -0700

    Mobile and Desktop Navigation files

---
+++ php/includes/mobile-navigation.php
@@ -0,0 +1,74 @@
+<div id="mobile-navbar-screen" class="screen mobile-menu-close visible-xs-block"></div>
+<div id="navbar-btn" class="mobile-menu-close visible-xs-block"><span class="glyphicon glyphicon-menu-hamburger"></span></div>
+<nav role="navigation" id="mobile-navbar" class="visible-xs-block">
+    <ul class="first-level" role="menu">
+        <li><h2>Aluminum</h2></li>
+        <li class="mobile-li" data-target="bar-stock-menu">Bar Stock</li>
+        <li class="mobile-li" data-target="clips-menu">Panel Rails & Clips</li>
+        <li class="mobile-li" data-target="fixturing-menu">Fixturing & Storefront</li>
+        <li class="mobile-li" data-target="caps-menu">Caps, Corners, Coves & Dividers</li>
+        <li class="mobile-li" data-target="cabinet-menu">Cabinet, Case & Closet</li>
+        <li class="mobile-li" data-target="trims-menu">Tee Molding & Edge Trims</li>
+        <li class="mobile-li" data-target="t-slot-menu">T-Slot Framing</li>
+        <li class="mobile-menu-close"><span class="glyphicon glyphicon-remove"></span><b> Close</b></li>
+    </ul>
+    <ul id="bar-stock-menu" class="second-level" role="menu">
+        <li class="mobile-back"><a href="#"><b>Return </b></a><span class="glyphicon glyphicon-menu-right"></span></li>
+        <li><a href="#"><span class="icon-angle"></span>Angles</a></li>
+        <li><a href="#"><span class="icon-flat"></span>Flat Bars</a></li>
+        <li><a href="#"><span class="icon-flat-radiused"></span>Flat Bars: Radiused</a></li>
+        <li><a href="#"><span class="icon-h-section"></span>H Sections</a></li>
+        <li><a href="#"><span class="icon-half-round"></span>Half Rounds</a></li>
+        <li><a href="#"><span class="icon-hex"></span>Hex Bars</a></li>
+        <li><a href="#"><span class="icon-square-hollow"></span>Square Tubes</a></li>
+        <li><a href="#"><span class="icon-round-rod"></span>Round Rod</a></li>
+        <li><a href="#"><span class="icon-round-tube"></span>Round Tubes</a></li>
+        <li><a href="#"><span class="icon-tee-bar"></span>Tee Bars</a></li>
+        <li><a href="#"><span class="icon-z-bar"></span>Z Bars</a></li>
+    </ul>
+    <ul id="clips-menu" class="second-level" role="menu">
+        <li class="mobile-back"><a href="#"><b>Return </b></a><span class="glyphicon glyphicon-menu-right"></span></li>
+        <li><a href="#"><span class="icon-clip-one"></span>Style 1</a></li>
+        <li><a href="#"><span class="icon-clip-two"></span>Style 2</a></li>
+        <li><a href="#"><span class="icon-clip-hd"></span>HD Panel Rail</a></li>
+        <li><a href="#"><span class="icon-clip-xl"></span>XL Panel Rail</a></li>
+        <li><a href="#"><span class="icon-z-hanger"></span>Z-Hanger</a></li>
+    </ul>
+    <ul id="fixturing-menu" class="second-level" role="menu">
+        <li class="mobile-back"><a href="#"><b>Return </b></a><span class="glyphicon glyphicon-menu-right"></span></li>
+        <li><a href="#"><span class="icon-ez"></span>EZ Slatwall System</a></li>
+        <li><a href="#"><span class="icon-price-tag"></span>Price Tag Molding</a></li>
+        <li><a href="#"><span class="icon-slatwall"></span>Slatwall System</a></li>
+        <li><a href="#"><span class="icon-slatwall-panel"></span>Slatwall Panel</a></li>
+        <li><a href="#"><span class="icon-snap-frame"></span>Snap Frames</a></li>
+    </ul>
+    <ul id="caps-menu" class="second-level" role="menu">
+        <li class="mobile-back"><a href="#"><b>Return </b></a><span class="glyphicon glyphicon-menu-right"></span></li>
+        <li><a href="#"><span class="icon-corners"></span>Corners</a></li>
+        <li><a href="#"><span class="icon-divider"></span>Divider Bars</a></li>
+        <li><a href="#"><span class="icon-j-cap"></span>J-Cap</a></li>
+    </ul>
+    <ul id="cabinet-menu" class="second-level" role="menu">
+        <li class="mobile-back"><a href="#"><b>Return </b></a><span class="glyphicon glyphicon-menu-right"></span></li>
+        <li><a href="#"><span class="icon-closet"></span>Closet Rod</a></li>
+        <li><a href="#"><span class="icon-tracks"></span>Door Track</a></li>
+        <li><a href="#"><span class="icon-rails"></span>File Hanging Rail</a></li>
+        <li><a href="#"><span class="icon-cabinet"></span>Garage Cabinet Trim</a></li>
+        <li><a href="#"><span class="icon-pulls"></span>Pulls and Handles</a></li>
+        <li><a href="#"><span class="icon-case"></span>Road Case Sections</a></li>
+    </ul>
+    <ul id="trims-menu" class="second-level" role="menu">
+        <li class="mobile-back"><a href="#"><b>Return </b></a><span class="glyphicon glyphicon-menu-right"></span></li>
+        <li><a href="#"><span class="icon-trim"></span>Edge Trim</a></li>
+        <li><a href="#"><span class="icon-trim-heavy"></span>Edge Trim Heavy Lip</a></li>
+        <li><a href="#"><span class="icon-trim-grooved"></span>Grooved Edge Trim</a></li>
+        <li><a href="#"><span class="icon-trim-round"></span>Round Edge Trim</a></li>
+        <li><a href="#"><span class="icon-trim-rounded"></span>Rounded Edge Trim</a></li>
+        <li><a href="#"><span class="icon-trim-grooved"></span>Tee Molding</a></li>
+    </ul>
+    <ul id="t-slot-menu" class="second-level" role="menu">
+        <li class="mobile-back"><a href="#"><b>Return </b></a><span class="glyphicon glyphicon-menu-right"></span></li>
+        <li><a href="#"><span class="icon-quad"></span>Quad Tracks</a></li>
+        <li><a href="#"><span class="icon-t-track"></span>T-Tracks</a></li>
+    </ul>
+</nav>
---
+++ php/includes/navbar.php
@@ -0,0 +1,79 @@
+<nav role="navigation" class="navbar" id="navbar">
+    <div class="navbar-collapse collapse">
+        <ul class="nav nav-justified">
+            <li>
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Bar Stock</a>
+                <ul class="dropdown-menu" role="menu">
+                    <li><a href="#">Angles</a></li>
+                    <li><a href="#">Flat Bars</a></li>
+                    <li><a href="#">Flat Bars: Radiused</a></li>
+                    <li><a href="#">H Sections</a></li>
+                    <li><a href="#">Half Rounds</a></li>
+                    <li><a href="#">Hex Bars</a></li>
+                    <li><a href="#">Square Tubes</a></li>
+                    <li><a href="#">Round Rod</a></li>
+                    <li><a href="#">Round Tubes</a></li>
+                    <li><a href="#">Tee Bars</a></li>
+                    <li><a href="#">Z Bars</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Panel Rails & Clips</a>
+                <ul class="dropdown-menu" role="menu">
+                    <li><a href="#">Style 1</a></li>
+                    <li><a href="#">Style 2</a></li>
+                    <li><a href="#">Z-Hanger</a></li>
+                    <li><a href="#">HD Panel Rail</a></li>
+                    <li><a href="#">XL Panel Rail</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Extruding and Storefront</a>
+                <ul class="dropdown-menu" role="menu">
+                    <li><a href="#">Ez Slatwall System</a></li>
+                    <li><a href="#">Price Tag Moldings</a></li>
+                    <li><a href="#">Slatwall System</a></li>
+                    <li><a href="#">Slatwall Panel</a></li>
+                    <li><a href="#">Snap Frames</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Caps, Corners, Coves and Dividers</a>
+                <ul class="dropdown-menu" role="menu">
+                    <li><a href="#">Corners</a></li>
+                    <li><a href="#">Divider Bar</a></li>
+                    <li><a href="#">J-Cap</a></li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Cabinet, Case & Closet</a>
+                <ul class="dropdown-menu" role="menu">
+                    <li><a href="#">Closet Rod</a></li>
+                    <li><a href="#">Door Tracks</a></li>
+                    <li><a href="#">File Hanging Rail</a></li>
+                    <li><a href="#">Garage Cabinet Trim</a></li>
+                    <li><a href="#">Pulls and Handles</a></li>
+                    <li><a href="#">Road Case Sections</a></li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Tee Molding & Edge Trims</a>
+                <ul class="dropdown-menu" role="menu">
+                    <li><a href="#">Edge Trim</a></li>
+                    <li><a href="#">Edge Trim Heavy Lip</a></li>
+                    <li><a href="#">Grooved Edge Trim</a></li>
+                    <li><a href="#">Round Edge Trim</a></li>
+                    <li><a href="#">Rounded Edge Trim</a></li>
+                    <li><a href="#">Tee Molding</a></li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">T-Slot Framing</a>
+                <ul class="dropdown-menu" role="menu">
+                    <li><a href="#">Quad-Tracks</a></li>
+                    <li><a href="#">T-Track</a></li>
+                </ul>
+            </li>
+        </ul>
+    </div><!--/.nav-collapse -->
+</nav>
