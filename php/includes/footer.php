4276cf69a025693aa88c1e81414d6bf5b7f7bf1b
oscarlomas09 <oscar-lomas09@hotmail.com>
2015-04-21 16:07:56 -0700

    Completed Footer

---
+++ php/includes/footer.php
@@ -0,0 +1,82 @@
+<footer id="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="col-sm-3">
+                    <h4>Contact Us</h4>
+                    <section>
+                        Orange Aluminum <br>
+                        P.O. Box 5961 <br>
+                        Orange, CA 92863-5961 <br><br>
+
+                        Local:(714) 464-2181 <br>
+                        Toll Free:(877) 464-2181 <br>
+                        Fax:(714) 771-1221 <br><br>
+
+                        <b>orders@orangealuminum.com</b>
+                    </section>
+                </div>
+                <div class="col-sm-2">
+                    <h4><a data-toggle="collapse" href="#footer-about" aria-expanded="true" aria-controls="footer-about">About Us</a></h4>
+                    <ul class="nav collapse" id="footer-about">
+                        <li><a href="#">Company Info</a></li>
+                        <li><a href="#">Company News</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-2">
+                    <h4><a data-toggle="collapse" href="#footer-help" aria-expanded="true" aria-controls="footer-help">Help</a></h4>
+                    <ul class="nav collapse" id="footer-help">
+                        <li><a href="#">FAQs</a></li>
+                        <li><a href="#">Price Match Guarantee</a></li>          
+                        <li><a href="#">My Account</a></li>          
+                        <li><a href="#">NET30 Application</a></li>          
+                    </ul>
+                </div>
+                <div class="col-sm-2">
+                    <h4><a data-toggle="collapse" href="#footer-shop" aria-expanded="true" aria-controls="footer-shop">Shop</a></h4>
+                    <ul class="nav collapse" id="footer-shop">
+                        <li><a href="#">Custom Extrusions</a></li>
+                        <li><a href="#">Stocking Program</a></li>
+                        <li><a href="#">Cut to Length</a></li>
+                        <li><a href="#">Volume Pricing</a></li>
+                        <li><a href="#">Advanced Search</a></li>
+                    </ul>
+                </div>  
+                <div class="col-sm-2">
+                   <h4><a data-toggle="collapse" href="#footer-info" aria-expanded="true" aria-controls="footer-info">Site Info</a></h4>
+                    <ul class="nav collapse" id="footer-info">
+                        <li><a href="#">Terms & Conditions</a></li>
+                        <li><a href="#">Privacy Policy</a></li>
+                        <li><a href="#">Site Map</a></li>
+                    </ul>
+                </div>  
+                <div id="social-menu" class="col-sm-1">
+                    <a><span class="icon-facebook"></span></a>
+                    <a><span class="icon-instagram"></span></a>
+                    <a><span class="icon-twitter"></span></a>
+                    <a><span class="icon-pinterest"></span></a>
+                </div> 
+            </div>
+        </div>
+        <div class="row hidden-xs" id="lower-footer">
+            <div class="col-lg-5">
+                <span>Newsletter Sign-Up</span><br>
+                <form class="form-inline">
+                    <div class="form-group">
+                        <label class="sr-only" for="newsletter-input">Newsletter Email</label>
+                        <input type="email" class="form-control" id="newsletter-input" placeholder="Email">
+                        <button type="submit" class="btn btn-success">Submit</button>
+                    </div>
+                </form>
+            </div>
+            <div class="col-lg-7 other-icons text-right">
+                <span class="icon-mastercard"></span>
+                <span class="icon-visa"></span>
+                <span class="icon-amex"></span>
+                <span class="icon-discover"></span>
+                <span class="icon-secure"></span><small>SSL</small>
+            </div>
+        </div>
+    </div>
+</footer>
