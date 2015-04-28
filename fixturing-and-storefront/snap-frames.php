b1dd560c0fa198e6d5a610a32e837b220e4deb4d
Oscar Lomas <oscar-lomas09@hotmail.com>
2015-04-27 22:19:07 -0700

    Snap Frames Page

---
+++ fixturing-and-storefront/snap-frames.php
@@ -0,0 +1,266 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Snap Frames</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="../apple-touch-icon.png">
+        
+        <!-- Bootstrap -->
+        <link href="../css/bootstrap.min.css" rel="stylesheet">
+        <link href="../css/bootstrap-theme.min.css" rel="stylesheet">
+        <link href="../css/icons.css" rel="stylesheet">
+        
+        <script src="../js/vendor/modernizr-2.8.3.min.js"></script>
+        <script src="../js/vendor/respond-.4.2.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        <?php include("../php/includes/header.php"); ?>
+        <?php include("../php/includes/navbar.php"); ?>
+        <?php include("../php/includes/mobile-navigation.php"); ?>
+        
+        <!-- Content Just for this Page -->
+        <div class="jumbotron"><img src="../img/jumbotron/Assembled_2.jpg" alt=""></div>
+        <main class="container-fluid common-container">           
+            <ol class="breadcrumb">
+              <li><a href="../index.php">Home</a></li>
+              <li><a href="../fixturing-and-storefront.php">Fixturing and Storefront</a></li>
+              <li>Snap Frames</li>
+            </ol>
+            <!-- Modal Window for Tolerance Table-->
+            <div class="modal fade" id="toleranceModal" tabindex="-1" role="dialog" aria-hidden="true">
+                <div class="modal-dialog modal-lg">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                            <h4 class="modal-../title" id="myMod../alLabel">Standard System of Measurement Inch Material</h4>
+                        </div>
+                        <div class="modal-body">
+                            <img src="../img/tolerance.jpg" alt="Tolerance Table" style="max-width:100%;"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="filter-btn" class="visible-xs" data-toggle="collapse" data-target="#filters" aria-expanded="false" aria-controls="filters"><span class="glyphicon glyphicon-tasks"></span> Filter</div>
+            <div class="row">
+                <aside class="col-xs-12 col-sm-3">
+                    <div class="filter clearfix" id="filters">
+                        <h3>Snap Frames <span class="filter-close glyphicon glyphicon-remove visible-xs" data-toggle="collapse" data-target="#filters"></span></h3>
+                        <section class="filter-content">
+                            <div class="cut filter-type">
+                               <span class="filter-title">Cut Length</span>
+                               <form id="cut-form">
+                                   <div class="filter-option">
+                                       <input type="radio" id="cut-eight" name="cut-length" value="fixed" checked/>
+                                       <label for="cut-eight"><span></span>8'</label>
+                                   </div>
+                               </form>
+                            </div>
+                            <div class="finish filter-type">
+                               <span class="filter-title">Finish</span>
+                               <form id="clips-finish-form">
+                                   <div class="filter-option">
+                                       <input type="radio" id="clear" name="finish" value="fixed" checked/>
+                                       <label for="clear"><span></span>Clear Anodized</label>
+                                   </div>
+                               </form>
+                            </div>
+                            <div class="alloy filter-type">
+                               <span class="filter-title">Alloy & Temper</span>
+                               <form id="clips-alloy-form">
+                                   <div class="filter-option">
+                                       <input type="radio" id="alloy" name="alloy" value="fixed" checked/>
+                                       <label for="alloy"><span></span>6063-T5</label>
+                                   </div>
+                               </form>
+                            </div>
+                            <div class="clearfix"></div>
+                            <div id="reset-btn" class="text-center clearfix">Reset Filters</div>
+                        </section>
+                    </div>
+                </aside>
+                <div class="col-xs-12 col-sm-9">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Product Info</h3>
+                        </div>
+                        <div class="panel-body container-fluid">
+                            <div class="row">
+                               <div class="col-xs-12">
+                                    <p>Specialty designed for the elite, American made, and Style with Function. We have designed one of the most popular rounded style snap frame extrusions on the market, easy on the eye and on your pocket book. They are sold in 8 Foot Lengths and include Corner Brackets and your required Spring Steel! We are sure that our Extruded Snap Frame Extrusions will add the style and function to the advertisements your client is looking for.</p>
+                                    <table style="width:100%;">
+                                        <tr>
+                                            <td><b>Alloy</b></td>
+                                            <td style="width:75%;">
+                                                <a data-toggle="tooltip" title="Ultra-Corrosive Resistant Architectural Grade Alloy">6063-T5</a>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td><b>Finish</b></td>
+                                            <td style="width:75%;">
+                                                <a data-toggle="tooltip" title="Clear Anodized Finish, Per MIL-A-8625F. Electrochemical process that is corrosion resistant and protects the material from oxidizing.">Clear Anodized</a></td>
+                                        </tr>
+                                        <tr>
+                                            <td><b>Cut Length</b></td>
+                                            <td style="width:75%;">8' Cut Lengths</td>
+                                        </tr>
+                                        <tr>
+                                            <td><b>Tolerance</b></td>
+                                            <td style="width:75%;">
+                                                <a data-toggle="modal" data-target="#toleranceModal">Standard System of Measurement Inch Material</a>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td><b>MET</b></td>
+                                            <td style="width:75%;">
+                                                <a data-toggle="tooltip" title="ASTM is the American Society for Testing and Materials.">ASTM</a>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- COVER -->
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Cover</h3>
+                        </div>
+                        <div class="panel-body container-fluid">
+                            <div class="row">
+                               <div class="col-xs-12 col-md-2">
+                                    <img class="img-tall" alt="" src="img/tile-tall.jpg"/>
+                                </div>
+                                <div class="col-xs-12 col-md-10 visible-md visible-lg">
+                                  <p>Clear Anodized Aluminum Snap Frame Cover Cap, engineered to easily hinge atop our Base Extrusion with precise accuracy. Sold in long lengths to allow you to meet the variety of printed advertisements your project may require.
+4 Corner brackets and 4 Spring Steels Included with every 8 foot pairing lengths</p>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <table class="table table-hover" style="width:100%;">
+                                  <thead>
+                                    <tr>
+                                      <th>SKU</th>
+                                      <th>Cut Length</th>
+                                      <th>Finish</th>
+                                      <th>Each</th>
+                                    </tr>
+                                  </thead>
+                                  <tbody>
+                                    <tr data-toggle="collapse" data-target="#collapse-OA9069" class="accordion-togglee">
+                                      <th scope="row">OA9069</td>
+                                      <td>8'</td>
+                                      <td>Clear Anodized</td>
+                                      <td>$12.86</td>
+                                    </tr>
+                                    <tr>
+                                        <td colspan="4" class="hiddenRow">
+                                            <div class="accordion-body collapse" id="collapse-OA9069" style="padding:8px 13px;">
+                                                <h4 class="text-center">Snap Frame:Cover</h4>
+                                                <form style="width:100%;margin:0px auto;">
+                                                   <a href="#" class="product-link">
+                                                       Product View<span class="glyphicon glyphicon-share"></span>
+                                                    </a>
+                                                    <div class="add-cart clearfix">                         
+                                                      <label class="sr-only">Quantity</label>    
+                                                      <input type="number" id="OA9069-qty" min="0" value="1">
+                                                      <button type="submit">
+                                                          <span class="glyphicon glyphicon-shopping-cart"></span> Add to Cart
+                                                      </button>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </td>
+                                    </tr>          
+                                  </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- BASE -->
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Base</h3>
+                        </div>
+                        <div class="panel-body container-fluid">
+                            <div class="row">
+                               <div class="col-xs-12 col-md-2">
+                                    <img class="img-tall" alt="" src="img/tile-tall.jpg"/>
+                                </div>
+                                <div class="col-xs-12 col-md-10 visible-md visible-lg">
+                                  <p>Clear Anodized Aluminum Snap Frame Base Plate, designed to easily work perfectly with our Cap profile, while engineered with the bottom mount T Track to allow for easier assembly and accommodation for our flush hanger. The OA9410 Hanger Extrusion quickly mounts to the wall while providing a functional tongue that serves as the hanging rail for the base to slide easily into. Sold in long lengths to allow you to meet the variety of printed advertisements your project may require.
+4 Corner brackets and 4 Spring Steels Included with every 8 foot pairing lengths</p>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <table class="table table-hover" style="width:100%;">
+                                  <thead>
+                                    <tr>
+                                      <th>SKU</th>
+                                      <th>Cut Length</th>
+                                      <th>Finish</th>
+                                      <th>Each</th>
+                                    </tr>
+                                  </thead>
+                                  <tbody>
+                                    <tr data-toggle="collapse" data-target="#collapse-OA9055" class="accordion-togglee">
+                                      <th scope="row">OA9055</td>
+                                      <td>8'</td>
+                                      <td>Clear Anodized</td>
+                                      <td>$14.72</td>
+                                    </tr>
+                                    <tr>
+                                        <td colspan="4" class="hiddenRow">
+                                            <div class="accordion-body collapse" id="collapse-OA9055" style="padding:8px 13px;">
+                                                <h4 class="text-center">Snap Frame:Base</h4>
+                                                <form style="width:100%;margin:0px auto;">
+                                                   <a href="#" class="product-link">
+                                                       Product View<span class="glyphicon glyphicon-share"></span>
+                                                    </a>
+                                                    <div class="add-cart clearfix">                         
+                                                      <label class="sr-only">Quantity</label>    
+                                                      <input type="number" id="OA9055-qty" min="0" value="1">
+                                                      <button type="submit">
+                                                          <span class="glyphicon glyphicon-shopping-cart"></span> Add to Cart
+                                                      </button>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </td>
+                                    </tr>          
+                                  </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        </main>
+        
+        <?php include("../php/includes/chat.php"); ?>
+        <?php include("../php/includes/footer.php"); ?>
+        
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="../js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+        <!-- Include all compiled plugins (below), or  individual files as needed -->
+        <script src="../js/vendor/bootstrap.min.js"></script>
+        <script src="../js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+        </script>
+    </body>
+</html>
