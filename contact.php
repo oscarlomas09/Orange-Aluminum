8834d4dcc8bc2796a7c19e5acad3c46f50c654a6
oscarlomas09 <oscar-lomas09@hotmail.com>
2015-04-30 11:09:44 -0700

    News and Contact Page

---
+++ contact.php
@@ -0,0 +1,108 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Company News</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/bootstrap-theme.min.css" rel="stylesheet">
+        <link href="css/icons.css" rel="stylesheet">
+        
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+        <script src="js/vendor/respond-1.4.2.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        <?php include("php/includes/header.php"); ?>
+        <?php include("php/includes/navbar.php"); ?>
+        <?php include("php/includes/mobile-navigation.php"); ?>
+        <div class="common-container container-fluid">
+            <div class="row">
+                <div class="col-xs-12 col-sm-9" style="font-size:1.2em;">
+                    <a class="btn btn-warning" href="index.php"><span class="glyphicon glyphicon-arrow-left">&nbsp;</span>Back to Shopping</a><br><br>
+                    <h2>Contact Us</h2><hr>
+                    <form>
+                        <table style="width:100%;" class="form-panel">
+                            <thead><th colspan="2">Contact Information</th></thead>
+                        </table>
+                        <div class="container-fluid form-panel">
+                            <div class="row">
+                                <div class="col-xs-12 col-sm-6">
+                                    <div>
+                                        <label>Inquiry*</label><br>
+                                        <select name="inquiry" id="inquiry" class="required-entry validate-select">
+                                            <option value="general_inquiries">General Inquiries</option>
+                                            <option value="order_inquiries">Order Inquiries</option>
+                                            < value="distribution_inquiries">Distribution Inquiries</option>
+                                            <option value="custom_profile_inquiry">Custom Profile Inquiry</option>
+                                            <option value="volume_discounts">Volume Discounts</option>
+                                            <option value="samplecatalog_request">Sample/Catalog Request</option>
+                                        </select>
+                                    </div>
+                                    <div>
+                                        <label>First Name</label><br>
+                                        <input type="text"/>
+                                    </div>
+                                    <div>
+                                        <label>Last Name</label><br>
+                                        <input type="text"/>
+                                    </div>
+                                    <div>
+                                        <label>Business Name</label><br>
+                                        <input type="text"/>
+                                    </div>
+                                    <div>
+                                        <label>Email*</label><br>
+                                        <input type="email" required/>
+                                    </div>
+                                    <div>
+                                        <label>Telephone</label><br>
+                                        <input type="tel"/>
+                                    </div>
+                                </div>
+                                <div class="col-xs-12 col-sm-6">                                    
+                                    <div>
+                                        <label>Comments</label><br>
+                                        <textarea rows="8"></textarea>
+                                    </div>
+                                </div>
+                            </div>
+                        </div><br>
+                        <button type="submit" class="btn btn-warning" style="font-size:1.2em;float:right;">Submit</button>
+                    </form>
+                </div>
+                <div class="hidden-xs col-sm-3">
+                    <?php include("php/includes/side-content.php"); ?>
+                </div>
+            </div>
+        </div>
+        <?php include("php/includes/chat.php"); ?>
+        <?php include("php/includes/footer.php"); ?>
+        
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+        <!-- Include all compiled plugins (below), or  individual files as needed -->
+        <script src="js/vendor/bootstrap.min.js"></script>
+        <script src="js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+        </script>
+    </body>
+</html>
---
+++ news.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Company News</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/bootstrap-theme.min.css" rel="stylesheet">
+        <link href="css/icons.css" rel="stylesheet">
+        
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+        <script src="js/vendor/respond-1.4.2.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        <?php include("php/includes/header.php"); ?>
+        <?php include("php/includes/navbar.php"); ?>
+        <?php include("php/includes/mobile-navigation.php"); ?>
+        <div class="common-container container-fluid">
+            <div class="row">
+                <div class="col-xs-12 col-sm-9" style="font-size:1.2em;">
+                    <a class="btn btn-warning" href="index.php"><span class="glyphicon glyphicon-arrow-left">&nbsp;</span>Back to Shopping</a><br><br>
+                    <h2>Company News</h2><hr>
+                    <p>
+                        Orange Aluminum Blog
+Our dedication to the industry is beyond just sourcing american made extruded products but also educating our customers in both the industry as well as the products themselves. If you have any specific products you would like to see reviewed by us, please let us know! We are here to help....
+                    </p>
+                    <div class="news clearfix">
+                        <img src="img/news-alloy.jpg" />
+                        <h4>Understanding 6000 Series Aluminum Alloys</h4>
+                        <span class="date">11/27/14</span>
+                        <p>In the United States, The Aluminum Association is responsible for the allocation and registration of aluminum alloys. Currently there are over 600 aluminum alloys registered with the Aluminum Association, so which one is right for you?</p>
+                        <a href="" class="view-more">View More >></a>
+                    </div><br>
+                    <div class="news clearfix">
+                        <img src="img/news-clips.jpg" />
+                        <h4>Hanging with Orange - Why Our Z Clips, Panel Hangers, Clips, etc...</h4>
+                        <span class="date">7/23/14</span>
+                        <p>Whether you are hanging your favorite piece of art, or thousands of mirrors for a hotel, Orange Aluminum is your source.  We know what you are looking for, have what you need, and the price you want to pay.  </p>
+                        <a href="" class="view-more">View More >></a>
+                    </div><br>
+                    <div class="news clearfix">
+                        <img src="img/news-site.jpg" />
+                        <h4>New Site is LIVE - Orange Aluminum a Fresh New Look</h4>
+                        <span class="date">7/21/14</span>
+                        <p>Orange Aluminum, the industry leader in Aluminum Extruded trims, extrusions, and moldiings, launches its new site.  Added features include increased searchability, NET 30 purchase at checkout and specialty pricing access for designated user accounts!</p>
+                        <a href="" class="view-more">View More >></a>
+                    </div>
+                </div>
+                <div class="hidden-xs col-sm-3">
+                    <?php include("php/includes/side-content.php"); ?>
+                </div>
+            </div>
+        </div>
+        <?php include("php/includes/chat.php"); ?>
+        <?php include("php/includes/footer.php"); ?>
+        
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+        <!-- Include all compiled plugins (below), or  individual files as needed -->
+        <script src="js/vendor/bootstrap.min.js"></script>
+        <script src="js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+        </script>
+    </body>
+</html>
