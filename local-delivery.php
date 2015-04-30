7972fd40c10178b83414b7cf7892f4094364a7cc
oscarlomas09 <oscar-lomas09@hotmail.com>
2015-04-30 10:25:23 -0700

    Same Day Delivery Page

---
+++ local-delivery.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>About Us</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/bootstrap-theme.min.css" rel="stylesheet">
+        <link href="css/icons.css" rel="stylesheet">
+        
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+        <script src="js/vendor/respond-1.4.2.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        <?php include("php/includes/header.php"); ?>
+        <?php include("php/includes/navbar.php"); ?>
+        <?php include("php/includes/mobile-navigation.php"); ?>
+        <div class="common-container container-fluid">
+            <div class="row">
+                <div class="col-xs-12 col-sm-9" style="font-size:1.2em;">
+                    <a class="btn btn-warning" href="index.php"><span class="glyphicon glyphicon-arrow-left">&nbsp;</span>Back to Shopping</a><br><br>
+                    <h2>Same Day Delivery</h2><hr>
+                    <div class="jumbotron" style="padding:0;">
+                        <img src="img/shipping-banner.jpg" alt="Shipping"/>
+                    </div>
+                    <div class="static-container">
+                        <p>Same Day Shipping is attempted by many, but how many can offer Same Day?<br><br>
+
+    At the end of the day what separates Orange Aluminum from our competition is our service.  We offer the most competitive pricing in the industry, but without prompt service the product is meaningless.  We understand the changing demands in the marketplace, and that sometimes you don't know what you need until the day you need it. <br><br>
+
+    Lucky for you, Orange offers Same Day delivery for most parts of <a href="zone.php">Orange and Los Angeles Counties</a>.  It is simple, order by 11 am, and we can have it on a truck for delivery by 5 pm the same day.  This is subject to material availability, but with our general level of inventory at any given time it is with confidence we are able to offer this service. Live just outside of our zone or in the Inland Empire? Give us a call for a delivery quote - we may be able to have your order delivered by 5 pm.<br><br>
+
+    You don't live in our current delivery regions?  Its okay, we still offer same day shipping for expedited orders and are more than willing to work with other shipping services that you may desire to arrange for transit.<br><br>
+
+
+
+    Call or email today to confirm availability of product and exceptions to 11 AM deadline.</p>
+                    </div>
+                </div>
+                <div class="hidden-xs col-sm-3">
+                    <?php include("php/includes/side-content.php"); ?>
+                </div>
+            </div>
+        </div>
+        <?php include("php/includes/chat.php"); ?>
+        <?php include("php/includes/footer.php"); ?>
+        
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+        <!-- Include all compiled plugins (below), or  individual files as needed -->
+        <script src="js/vendor/bootstrap.min.js"></script>
+        <script src="js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+        </script>
+    </body>
+</html>
---
+++ zone.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>About Us</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/bootstrap-theme.min.css" rel="stylesheet">
+        <link href="css/icons.css" rel="stylesheet">
+        
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+        <script src="js/vendor/respond-1.4.2.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        <?php include("php/includes/header.php"); ?>
+        <?php include("php/includes/navbar.php"); ?>
+        <?php include("php/includes/mobile-navigation.php"); ?>
+        <div class="container-fluid common-container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-9" style="font-size:1.2em;">
+                    <a class="btn btn-warning" href="index.php"><span class="glyphicon glyphicon-arrow-left">&nbsp;</span>Back to Shopping</a><br><br>
+                    <h2>Same Day Delivery Zone</h2><hr>
+                    <p>It is simple: order by 11 am and we can have it on a truck for delivery by 5 pm the same day! Live just outside of our zone or in the Inland Empire? Give us a call for a delivery quote - we may be able to have your order delivered by 5 pm.<br><br>
+
+Most orders are only $15!</p>
+                    
+                    <div class="jumbotron" style="padding:0;">
+                        <img src="img/delivery-zone.jpg" alt="Shipping"/>
+                    </div>
+                </div>
+                <div class="hidden-xs col-sm-3">
+                    <?php include("php/includes/side-content.php"); ?>
+                </div>
+            </div>
+        </div>
+        <?php include("php/includes/chat.php"); ?>
+        <?php include("php/includes/footer.php"); ?>
+        
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+        <!-- Include all compiled plugins (below), or  individual files as needed -->
+        <script src="js/vendor/bootstrap.min.js"></script>
+        <script src="js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+        </script>
+    </body>
+</html>
